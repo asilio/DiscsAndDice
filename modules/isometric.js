6199e4e916d266d7daf67bfd0bd5c1d68126da65
cwpersonrennell <cpersonrennell@gmail.com>
2025-03-07 14:17:54 -0700

    Update isometric.js

--- modules/isometric.js
+++ modules/isometric.js
@@ -168,17 +168,19 @@ class World{
 	drawGridToScreenAt(context,screen_target){
 		for(let x_world = 0; x_world<this.width;x_world++){
 			for(let y_world = 0;y_world<this.length;y_world++){
-				for(let z_world = 0; z_world <2; z++)
-				 context.fillStyle = `rgb(
-					${Math.floor(255 - 200/this.width * x_world)}
-					${Math.floor(255 - 200/this.length * y_world)}
-					0)`;
-				let x,y;
-				[x,y]=this.WorldToPixelCoordinate([x_world,y_world],screen_target);
-				context.beginPath();
-				context.arc(x,y,2,0,2*PI);
-				context.fill();
-				context.closePath();
+				for(let z_world = 0; z_world <2; z_world++){
+					 context.fillStyle = `rgb(
+						${Math.floor(255 - 200/this.width * x_world)}
+						${Math.floor(255 - 200/this.length * y_world)}
+						${Math.floor(0 + 100 * z_world)})`;
+
+					let x,y;
+					[x,y]=this.WorldToPixelCoordinate([x_world,y_world,z_world],screen_target);
+					context.beginPath();
+					context.arc(x,y,2,0,2*PI);
+					context.fill();
+					context.closePath();
+				}
 			}
 		}
 	}
